39d627861f75a132a3f70b717ad460c82a91c5c2
Andranik Arakelyan <andranikarakelyan365@gmail.com>
2023-07-10 00:10:20 +0400

    PR improved

--- homework-3/scripts/renderTriangle.js
+++ homework-3/scripts/renderTriangle.js
@@ -1,27 +1,29 @@
 function createTriangle(num) {
+  let inner = "";
   for (let i = 0; i < num; i++) {
-    let inner = "";
     for (let j = 0; j < num - i - 1; j++) {
       inner += " ";
     }
     for (let k = num - i - 1; k < num; k++) {
       inner += " *";
     }
-    preNode.innerText += inner + "\n";
+    inner += "\n";
   }
+  return inner;
 }
 
 function rotateTriangle(num) {
+  let inner = "";
   for (let i = num; i > 0; i--) {
-    let inner = "";
     for (let j = i; j < num; j++) {
       inner += " ";
     }
     for (let k = num - i; k < num; k++) {
       inner += " *";
     }
-    reversedPreNode.innerText += inner + "\n";
+    inner += "\n";
   }
+  return inner;
 }
 
 const inputNode = document.getElementById("input");
@@ -30,16 +32,15 @@ const reversedPreNode = document.getElementById("reverse-triangle");
 const buttonNode = document.getElementById("rotate");
 
 buttonNode.addEventListener("click", () => {
-  reversedPreNode.innerText = "";
-  rotateTriangle(inputNode.value);
+  reversedPreNode.innerText = rotateTriangle(inputNode.value);
 });
 
 inputNode.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    preNode.innerText = "";
+    preNode.innerText = createTriangle(inputNode.value);
     reversedPreNode.innerText = "";
-    createTriangle(inputNode.value);
+
     buttonNode.style.display = "block";
   }
 });
