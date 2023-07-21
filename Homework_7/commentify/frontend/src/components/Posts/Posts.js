b7cd3c4585405ebeeeae1399cd8bc885a94c911b
Andranik Arakelyan <andranikarakelyan365@gmail.com>
2023-07-21 15:14:07 +0400

    changed Header.js and Posts.js to functional

--- Homework_7/commentify/frontend/src/components/Posts/Posts.js
+++ Homework_7/commentify/frontend/src/components/Posts/Posts.js
@@ -1,59 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 import { Post } from "../../components";
 import Pagination from "@mui/material/Pagination";
 
 import classes from "./Posts.module.css";
 
-class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.postEachPage = 3;
-    this.state = {
-      currentPage: 1,
-    };
-  }
+function Posts(props) {
+  const postEachPage = 3;
+  const [currentPage, setCurrentPage] = useState(1);
 
-  drawPosts = (posts) => {
+  const drawPosts = (posts) => {
     return posts.map((post) => {
       return <Post key={post.id} post={post} id={post.id} />;
     });
   };
 
-  drawPageNumbers = (n) => {
+  const changePage = (e, pageNumber) => {
+    if (pageNumber !== currentPage) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  const drawPageNumbers = (n) => {
     return (
       <Pagination
         sx={{ display: "flex", justifyContent: "center" }}
         count={n}
-        page={this.state.currentPage}
-        onChange={this.changePage}
+        page={currentPage}
+        onChange={changePage}
       />
     );
   };
 
-  changePage = (e, pageNumber) => {
-    if (pageNumber !== this.state.currentPage) {
-      this.setState({
-        currentPage: pageNumber,
-      });
-    }
-  };
+  const totalPages = Math.ceil(props.posts.length / postEachPage);
+  const showedPosts = props.posts.slice(
+    (currentPage - 1) * postEachPage,
+    currentPage * postEachPage
+  );
 
-  render() {
-    const totalPages = Math.ceil(this.props.posts.length / this.postEachPage);
-    const showedPosts = this.props.posts.slice(
-      (this.state.currentPage - 1) * this.postEachPage,
-      this.state.currentPage * this.postEachPage
-    );
-    return (
-      <section className={classes.container}>
-        <ul className={classes.posts}>{this.drawPosts(showedPosts)}</ul>
-        <ul style={{ marginTop: "40px" }}>
-          {this.drawPageNumbers(totalPages)}
-        </ul>
-      </section>
-    );
-  }
+  return (
+    <section className={classes.container}>
+      <ul className={classes.posts}>{drawPosts(showedPosts)}</ul>
+      <ul style={{ marginTop: "40px" }}>{drawPageNumbers(totalPages)}</ul>
+    </section>
+  );
 }
 
 export default Posts;
